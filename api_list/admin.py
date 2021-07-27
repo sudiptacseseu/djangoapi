f2b9a7d7d9875a967d082521d7a4179b77bed826
Sudipta Karmakar <sudiptacse.seu@gmail.com>
2021-07-27 21:57:11 +0600

    New app created called api_list with new model class named Course.

---
+++ api_list/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from api_list.models import Course
+
+admin.site.register(Course)
---
+++ api_list/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from api_list import views
+
+urlpatterns = [
+    # path('', views.home),
+]
